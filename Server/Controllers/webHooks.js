5357b44a425d4be03ded7c8efec1e03d917b0ea9
KaranMaggo <karanmaggo010@gmail.com>
2025-06-21 00:53:04 +0530

    corrected user creation

--- Server/Controllers/webHooks.js
+++ Server/Controllers/webHooks.js
@@ -14,10 +14,10 @@ export const clerkWebHooks = async (req, res) => {
     });
     const { data, type } = req.body;
     switch (type) {
-      case "User.created": {
+      case "user.created": {
         const userData = {
           _id: data.id,
-          email: data.email_address[0].email_address,
+          email: data.email_addresses[0].email_address,
           name: data.first_name + " " + data.last_name,
           imageUrl: data.image_url,
         };
@@ -26,7 +26,7 @@ export const clerkWebHooks = async (req, res) => {
         break;
       }
 
-      case "User.updated": {
+      case "user.updated": {
         const userData = {
           email: data.email_address[0].email_address,
           name: data.first_name + "" + data.last_name,
@@ -36,7 +36,7 @@ export const clerkWebHooks = async (req, res) => {
         res.json({});
         break;
       }
-      case "User.deleted": {
+      case "user.deleted": {
         await User.findByIdAndDelete(data.id);
 
         res.json({});
